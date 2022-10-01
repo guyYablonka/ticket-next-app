4d6474e2db2280aaf99c7e8697d4a4114990c5a9
guyYablonka <guyyablonka@gmail.com>
2022-10-01 18:12:58 +0300

    add code

---
+++ pages/_app.js
@@ -0,0 +1,28 @@
+import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
+import Header from "../components/Header";
+
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <div className="p-5">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
+};
+
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/currentuser");
+
+  let pageProps;
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return { pageProps, ...data };
+};
+
+export default AppComponent;
---
+++ pages/auth/signup.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import Router from "next/router";
+import useRequest from "../../hooks/use-request";
+
+export default () => {
+  const [email, setEamil] = useState("");
+  const [password, setPassword] = useState("");
+  const { doRequest, errors } = useRequest({
+    url: "/api/users/signup",
+    method: "post",
+    body: { email, password },
+    onSuccess: () => Router.push("/"),
+  });
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    doRequest();
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign Up</h1>
+      <div className="form-group">
+        <label>Email address</label>
+        <input
+          className="form-control"
+          value={email}
+          onChange={(e) => setEamil(e.target.value)}
+        ></input>
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          type="password"
+          className="form-control"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+      </div>
+      {errors}
+      <button className="btn btn-primary">Sign Up</button>
+    </form>
+  );
+};
